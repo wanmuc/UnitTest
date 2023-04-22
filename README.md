008c37122120ab6712e2390d1d02254b073c8acd
wanmuc <769535441@qq.com>
2023-04-22 22:41:28 +0800

    提交第一版的单元测试框架代码

---
+++ README.md
@@ -0,0 +1,22 @@
+# 1.UnitTest
+> 使用c++实现的轻量级的单元测试框架。
+
+# 2.支持的断言宏
+- ASSERT_TRUE
+- ASSERT_FALSE
+- ASSERT_EQ
+- ASSERT_NE
+- ASSERT_LT
+- ASSERT_LE
+- ASSERT_GT
+- ASSERT_GE
+
+# 3.其他特性
+- 支持对要执行的单元测试用例进行过滤，支持正则表达式的过滤。
+
+# 4.实现原理
+- 在UnitTestCore.hpp的头文件中，定义了TestCase类和UnitTestCore类。
+- TestCase类代表了每个不同的测试用例，具体的测试用例类需要继承TestCase类。
+- UnitTestCore类实现了测试用例的注册和执行。
+- TEST_CASE宏实现了测试用例类的声明和Run函数的展开。
+- RUN_ALL_TESTS宏实现了单元测试程序的执行。
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#include "UnitTestCore.hpp"
+
+using namespace std;
+
+int func(int value) { return value; }
+
+TEST_CASE(assert_eq) {
+  ASSERT_EQ(func(1), 1);
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(1 + 2, 1 + 1 + 1);
+}
+
+TEST_CASE(assert_true) {
+  ASSERT_TRUE(true);
+  ASSERT_TRUE(10 > 2);
+  ASSERT_TRUE(func(1) == 1);
+}
+
+TEST_CASE(assert_false) {
+  ASSERT_FALSE(false);
+  ASSERT_FALSE(2 > 10);
+  ASSERT_FALSE(func(1) != 1);
+}
+
+TEST_CASE(assert_ne) {
+  ASSERT_NE(10, 11);
+  ASSERT_NE(10 + 10, 11 + 11);
+  ASSERT_NE(func(1), func(1) + func(9));
+}
+
+TEST_CASE(assert_gt) {
+  ASSERT_GT(1, 0);
+  ASSERT_GT(func(1), 0);
+  ASSERT_GT(func(3), func(1) + func(1));
+}
+
+TEST_CASE(assert_ge) {
+  ASSERT_GE(1, 1);
+  ASSERT_GE(func(1), 1);
+  ASSERT_GE(func(3), func(1) + func(1) + func(1));
+}
+
+TEST_CASE(assert_lt) {
+  ASSERT_LT(0, 1);
+  ASSERT_LT(func(1), 2);
+  ASSERT_LT(func(3), func(1) + func(1) + func(1) + func(1));
+}
+
+TEST_CASE(assert_le) {
+  ASSERT_LE(0, 1);
+  ASSERT_LE(1, 1);
+  ASSERT_LE(func(2), 2);
+  ASSERT_LE(func(3), func(1) + func(1) + func(1));
+}
+
+RUN_ALL_TESTS();
